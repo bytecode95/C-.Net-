6ffd3a80e78145594a9b0d27ae6535aacdd13b91
chethanavirajini <cheth.virajini@gmail.com>
2023-11-06 18:52:29 +0530

    updated

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+//inside the class main method - that is the entry point to the application
+//CLR execute the code inside the main method
+namespace HelloWorld 
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
