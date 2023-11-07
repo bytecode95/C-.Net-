3f7a9ec502db4fd4271131add74361b52b736551
chethanavirajini <cheth.virajini@gmail.com>
2023-11-07 18:01:52 +0530

    comment

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+//there may be different classes in namespace. if we are using defined class we have to import it using statment
 //inside the class main method - that is the entry point to the application
 //CLR execute the code inside the main method
 namespace HelloWorld 
