72a9fea45b0a48d84ce7530547a87b9309cb5605
chethanavirajini <cheth.virajini@gmail.com>
2023-11-14 20:00:28 +0530

    datatypes

---
+++ overview/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 1;
+char c = 'A';
+float f = 123.45f;
+decimal d = 400.85m;
+int b = default;
+bool tf = default;
+
+Console.WriteLine($"{a}, {b}, {tf}, {c}, {f}, {d}");
+//implicit type convesion and type casting
+Console.WriteLine($"{c+a}");
+Console.WriteLine($"{(char)(c+a)}");
